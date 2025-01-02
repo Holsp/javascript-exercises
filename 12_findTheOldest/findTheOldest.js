31af15fd0637c4f58eabe54698b54ae999c3ede7
Holsp <Holsp@users.noreply.github.com>
2025-01-02 11:00:24 +0100

    Finished all base excersises

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
-
+const findTheOldest = function( people ) {
+   
+   let best;
+   let bestLifespan = 0;
+   
+   for( const person of people )
+   {
+      let lifespan;
+      if( person.yearOfDeath !== undefined )
+         lifespan =  person.yearOfDeath - person.yearOfBirth;         
+      else
+         lifespan = (new Date).getFullYear() - person.yearOfBirth;
+      
+      if( lifespan > bestLifespan )
+      {
+         best = person;
+         bestLifespan = lifespan;
+      }
+   }
+   return best;
 };
 
 // Do not edit below this line

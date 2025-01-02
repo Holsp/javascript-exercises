cec3a4cf631921f57671766d98ac13fc2c9e2df2
Holsp <Holsp@users.noreply.github.com>
2025-01-02 10:45:17 +0100

    Finished excersise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,8 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+   return array.reduce(( arr, object) => {
+      arr.push(object.title);
+      return arr;
+   }, [] );
 };
 
 // Do not edit below this line
